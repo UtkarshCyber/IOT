8ed571bc07eed95320a1fdfb61b24517e85cc610
UtkarshCyber <83854791+UtkarshCyber@users.noreply.github.com>
2023-02-01 12:31:37 +0530

    Create Home_automation_led.c

---
+++ lab15/Home_automation_led.c
@@ -0,0 +1,24 @@
+#define ledPin 13
+int state = 0;
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+digitalWrite(ledPin, LOW);
+Serial.begin(38400);
+}
+void loop()
+{
+if(Serial.available() > 0)
+if (state == '0')
+{
+digitalWrite(ledPin, LOW);
+Serial.println("LED: OFF");
+state = 0;
+}
+else if (state == '1')
+{
+digitalWrite(ledPin, HIGH);
+Serial.println("LED: ON");;
+state = 0;
+}
+}
